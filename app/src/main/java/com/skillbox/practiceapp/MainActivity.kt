0ab98866960da4360ed8bb0a4cf47ed4d0b48130
FockRock <smirnov_nikita@mail.ru>
2021-05-30 23:59:11 +0300

    App for progress calculation created

--- app/src/main/java/com/skillbox/practiceapp/MainActivity.kt
+++ app/src/main/java/com/skillbox/practiceapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.skillbox.practiceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skillbox.practiceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        val lessons = 25
+        val watched = 10
+        val result = (100/lessons)*watched
+
+        bindingClass.calculate.setOnClickListener {
+            bindingClass.lessonNumber.text = lessons.toString()
+            bindingClass.watchNumber.text = watched.toString()
+            bindingClass.progress.text = "$result%"
+        }
     }
 }
