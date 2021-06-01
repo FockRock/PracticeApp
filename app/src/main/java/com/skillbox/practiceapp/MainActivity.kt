9691dd3de8c1398e0929934d12d4e238fb074784
FockRock <smirnov_nikita@mail.ru>
2021-06-01 22:21:50 +0300

    App for progress calculation created Colors added

--- app/src/main/java/com/skillbox/practiceapp/MainActivity.kt
+++ app/src/main/java/com/skillbox/practiceapp/MainActivity.kt
@@ -15,12 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         val lessons = 25
         val watched = 10
-        val result = (100/lessons)*watched
+        val result = ((100/lessons)*watched)
 
         bindingClass.calculate.setOnClickListener {
             bindingClass.lessonNumber.text = lessons.toString()
             bindingClass.watchNumber.text = watched.toString()
             bindingClass.progress.text = "$result%"
+            bindingClass.progress.setBackgroundColor(
+                when(result){
+                in 0..19 -> getColor(R.color.black)
+                in 20..39 -> getColor(R.color.red)
+                in 40..79 -> getColor(R.color.yellow)
+                in 80..100 -> getColor(R.color.green)
+                else -> getColor(R.color.white)
+            }
+                )
         }
     }
 }
