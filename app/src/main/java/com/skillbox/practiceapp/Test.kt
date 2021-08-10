7e9fb6126967409a8d2fee31652b270fe62618f8
FockRock <smirnov_nikita@mail.ru>
2021-08-10 21:29:45 +0300

    test activity & test layout created

---
+++ app/src/main/java/com/skillbox/practiceapp/Test.kt
@@ -0,0 +1,13 @@
+package com.skillbox.practiceapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Test: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test)
+    }
+}
