50a4a8e3645e514e0928047235ea901a3d39bc38
FockRock <smirnov_nikita@mail.ru>
2021-08-13 00:12:57 +0300

    binding added

--- app/src/main/java/com/skillbox/practiceapp/Test.kt
+++ app/src/main/java/com/skillbox/practiceapp/Test.kt
@@ -3,11 +3,15 @@ package com.skillbox.practiceapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.skillbox.practiceapp.databinding.TestBinding
 
 class Test: AppCompatActivity() {
 
+    lateinit var bindingClass: TestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test)
+        bindingClass = TestBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
     }
 }
