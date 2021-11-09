393534e42334ae36339f1d42937cd71278e73405
FockRock <smirnov_nikita@mail.ru>
2021-11-09 22:37:44 +0300

    Fragment 2 added

--- app/src/main/java/com/skillbox/practiceapp/Fragments.kt
+++ app/src/main/java/com/skillbox/practiceapp/Fragments.kt
@@ -1,21 +1,24 @@
 package com.skillbox.practiceapp
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.skillbox.practiceapp.databinding.FragmentsBinding
 
 class Fragments: AppCompatActivity() {
 
-    lateinit var binding: FragmentsBinding
+    lateinit var bindingClass: FragmentsBinding
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        binding = FragmentsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = FragmentsBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
 
         openFragment(BlankFragment.newInstance(), R.id.fragmentContainer)
+
+        bindingClass.bSwitch.setOnClickListener{
+            openFragment(BlankFragment2.newInstance(), R.id.fragmentContainer)
+        }
     }
 
     private fun openFragment(f: Fragment, idHolder: Int) {
